3a5d9e70633ba26f67502b1fbd85523f182b9b1a
Rob-Yoo <jin12243@naver.com>
2024-06-01 13:06:52 +0900

    트레블 톡 환경 설정

---
+++ Travel_Talk/Travel_Talk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel_Talk
+//
+//  Created by Jinyoung Yoo on 5/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
