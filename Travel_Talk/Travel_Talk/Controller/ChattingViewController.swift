725493fe802923ab7bf67b0e93107d903ebe196d
Rob-Yoo <jin12243@naver.com>
2024-06-03 23:25:17 +0900

    메세지 입력창 레이아웃 적용 완료

--- Travel_Talk/Travel_Talk/Controller/ChattingViewController.swift
+++ Travel_Talk/Travel_Talk/Controller/ChattingViewController.swift
@@ -9,8 +9,23 @@ import UIKit
 
 class ChattingViewController: UIViewController {
 
+    @IBOutlet var messageTextFieldContainerView: UIView!
+    @IBOutlet var messageTextField: UITextField!
+    @IBOutlet var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.configureMessageTextFieldContainerView()
+    }
+}
 
+extension ChattingViewController {
+    private func configureMessageTextFieldContainerView() {
+        self.messageTextFieldContainerView.layer.cornerRadius = 10
+        self.messageTextField.borderStyle = .none
+        self.messageTextField.textColor = .black
+        self.messageTextField.attributedPlaceholder = NSAttributedString(string: "메세지를 입력하세요", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        self.sendButton.tintColor = .lightGray
     }
 }
